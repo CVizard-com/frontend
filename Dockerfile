5721ed4040a958f2386c6035d415bb383b5a90e5
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-24 13:48:58 +0200

    dockerize frontend

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM node:16-alpine
 
-EXPOSE 5173
-
+EXPOSE 5173:5173
 
 WORKDIR /app
 
-COPY cvizard/package.json .
+COPY cvizard/package.json /app/package.json
+COPY cvizard/package-lock.json /app/package-lock.json
 
-RUN npm install
+
+# RUN npm install
 
 COPY . . 
 
-CMD ["npm", "run", "dev"]
+CMD ["cd","cvizard","&&","npm","i","&&","npm", "run", "dev"]
--- cvizard/vite.config.js
+++ cvizard/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,6 @@ export default defineConfig({
     },
     host: true,
     strictPort: true,
-    port: 443
-  }
-})
+    port: 5173,
+  },
+});
