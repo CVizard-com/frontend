20f31fe48f8176d0a7c20583c6a43af61b1d7a48
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-25 20:41:00 +0200

    add https communication

--- cvizard/src/service/file.jsx
+++ cvizard/src/service/file.jsx
@@ -3,5 +3,5 @@ import axios from "axios";
 export async function uploadFile({ file }) {
   const formData = new FormData();
   formData.append("pdf_file", file);
-  await axios.post("https://localhost:8443/api/reader", formData);
+  await axios.post("https://cvizard.com:8443/api/reader", formData);
 }
