e37534399674b8df901a7c4199d6376533fc17ac
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-25 09:57:00 +0200

    baner component

---
+++ cvizard/src/baner.jsx
@@ -0,0 +1,16 @@
+export function Baner() {
+  return (
+    <div className="text-center">
+      <p className="text-lg font-medium leading-8 text-cyan-500">
+        Introducing CVizard
+      </p>
+      <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">
+        Anonymize and convert every CV
+      </h1>
+      <p className="mt-3 text-lg leading-relaxed text-slate-400 py-6">
+        Specify helps you unify your brand identity by collecting, storing and
+        distributing design tokens and assets — automatically.
+      </p>
+    </div>
+  );
+}
