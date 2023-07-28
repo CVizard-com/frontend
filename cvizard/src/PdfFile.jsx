ba5c690766c68a2ce0917f1518d05ede5f7e722f
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2023-07-28 09:58:14 +0200

    Update PdfFile.jsx

--- cvizard/src/PdfFile.jsx
+++ cvizard/src/PdfFile.jsx
@@ -21,10 +21,10 @@ export function PdfFile({ id, name, status, file, deleteFile }) {
 
         <button
           onClick={() => deleteFile(id)}
-          //   disabled={status !== "pending"}
-          disabled={status === "done" ? true : false}
+             
+          
           className={`inline-flex items-center justify-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${
-            status === "done"
+            false
               ? "bg-gray-400 text-gray-200 ring-gray-600/10"
               : "bg-red-100 text-red-700 ring-red-600/10"
           }`}
