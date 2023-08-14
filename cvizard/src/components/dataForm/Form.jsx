e94534bf124f167fc41c51cbfa636f802ea87d23
Paweł Solecki <pawelll.solecki@gmail.com>
2023-08-14 13:13:08 +0200

    style status

--- cvizard/src/components/dataForm/Form.jsx
+++ cvizard/src/components/dataForm/Form.jsx
@@ -8,6 +8,14 @@ export function Form({ file }) {
   const getCurrentIndex = () => {
     return files.findIndex((e) => e === file);
   };
+  const phrases = [
+    "Keep going!🔥",
+    "You're doing great!💪🏾",
+    "Almost there!😀",
+    "Stay strong, keep on!🏋🏻",
+    "You've got this—persist!🔥",
+    "Success is your journey's end!🌅",
+  ];
 
   return (
     <div className="grid grid-cols-2 w-8/12 h-5/6">
@@ -17,8 +25,11 @@ export function Form({ file }) {
       <FormList fieldName="email" file={file} />
       <FormList fieldName="website" file={file} />
       <FormList fieldName="other" file={file} />
-      <div>
-        {getCurrentIndex() + 1}/{files.length}
+      <div className="flex items-center justify-center w-full px-20 text-slate-400">
+        <b>
+          {getCurrentIndex()}/{files.length}
+        </b>
+        &nbsp; files are done. {phrases[getCurrentIndex() % phrases.length]}
       </div>
       <ProcessButton activeFile={file} />
     </div>
